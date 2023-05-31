91635bfbe35177f4944119966ba934e03c6215b8
unknown <alekseyleha@mail.ru>
2023-06-01 00:04:17 +0400

    add table with pagination and sorting for every columns

--- src/components/ListOfItems.tsx
+++ src/components/ListOfItems.tsx
@@ -2,33 +2,47 @@ import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import { SalesItem } from "@/api";
 import data from "@/data/sales.json";
-import { DatePicker, List } from "antd";
+import { DatePicker, Table } from "antd";
 
 const dateFormat = "DD/MM/YYYY";
+const columns = [{
+  title: 'Размер товара',
+  dataIndex: 'techSize',
+  key: 'techSize', defaultSortOrder: 'descend',
+  sorter: (a, b) => a.techSize - b.techSize,
+}, {
+  title: 'Артикул продавца',
+  dataIndex: 'supplierArticle',
+  key: 'supplierArticle',
+  sorter: (a, b) => {
+    return ('' + a.supplierArticle).localeCompare(b.supplierArticle);
+  },
+}, {
+  title: 'Бар-код',
+  dataIndex: 'barcode',
+  key: 'barcode',
+  sorter: (a, b) => a.barcode - b.barcode,
+}, {
+  title: 'К перечислению продавцу',
+  dataIndex: 'forPay',
+  key: 'forPay',
+  sorter: (a, b) => a.forPay - b.forPay
+}, {
+  title: 'Фактическая цена заказа',
+  dataIndex: 'finishedPrice',
+  key: 'finishedPrice',
+  sorter: (a, b) => a.finishedPrice - b.finishedPrice
+}]
 
 function ListOfItems() {
-  // console.log(data.data.products);
   const [itemsList, setItemsList] = useState<SalesItem[]>([]);
   const [selectedDate, setSelectedDate] = useState("22/05/2023");
 
-  // const currentDate = new Date(selectedDate);
-  // const currentDate2 = dayjs(selectedDate, dateFormat);
-
-  // console.log(currentDate2.month(), currentDate2.day());
-
   useEffect(() => {
-    // return;
     const currentDate = dayjs(selectedDate, dateFormat);
     const products = data as unknown as SalesItem[];
     const filteredProducts = products.filter((product) => {
       const productDate = new Date(product.date!);
-
-      console.log(
-        productDate.getMonth(),
-        currentDate.month(),
-        productDate.getDate(),
-        currentDate.date()
-      );
       return (
         productDate.getMonth() === currentDate.month() &&
         productDate.getDate() === currentDate.date()
@@ -48,18 +62,7 @@ function ListOfItems() {
         format={dateFormat}
       />
       <div>Count:{itemsList.length}</div>
-      <List
-        size="small"
-        header={<div>Header</div>}
-        bordered
-        dataSource={itemsList}
-        renderItem={(item) => (
-          <List.Item>
-            supplierArticle:{item.supplierArticle}| forPay:{item.forPay}|
-            finishedPrice:{item.finishedPrice}
-          </List.Item>
-        )}
-      />
+      <Table dataSource={itemsList} columns={columns} />
     </div>
   );
 }
