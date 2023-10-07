fea9eaa9912f4d2bdaa1bc1bbe3ea295ea6e48be
Aleks <alekseyleha@mail.ru>
2023-10-08 00:23:58 +0400

    fix url generator for image

--- src/constants/columns/reportDetailColumns.tsx
+++ src/constants/columns/reportDetailColumns.tsx
@@ -4,11 +4,25 @@ import { ColumnType } from "antd/es/table";
 
 export type ReportColumnType = typeof reportDetailColumns;
 
-function getImgRef(id:number){
-    const idAsString = String(id);
-    const vol = idAsString.substring(0, 3);
-    const part = idAsString.substring( 3);
-    return `https://basket-03.wb.ru/vol${vol}/part${part}/${idAsString}/images/c516x688/1.webp`
+function getHost(id:number) {
+    if (id >= 0 && id <= 143) return "//basket-01.wb.ru";
+    if (id >= 144 && id <= 287) return "//basket-02.wb.ru";
+    if (id >= 288 && id <= 431) return "//basket-03.wb.ru";
+    if (id >= 432 && id <= 719) return "//basket-04.wb.ru";
+    if (id >= 720 && id <= 1007) return "//basket-05.wb.ru";
+    if (id >= 1008 && id <= 1061) return "//basket-06.wb.ru";
+    if (id >= 1062 && id <= 1115) return "//basket-07.wb.ru";
+    if (id >= 1116 && id <= 1169) return "//basket-08.wb.ru";
+    if (id >= 1170 && id <= 1313) return "//basket-09.wb.ru";
+    if (id >= 1314 && id <= 1601) return "//basket-10.wb.ru";
+    if (id >= 1602 && id <= 1655) return "//basket-11.wb.ru";
+    return "//basket-12.wb.ru";
+  }
+
+function getImgRef(id:number){ 
+    const vol = ~~(id/1e5);
+    const part = ~~(id/1e3);
+    return `https:${getHost(vol)}/vol${vol}/part${part}/${id}/images/c516x688/1.webp`
 }
 
 export const reportDetailColumns = [
@@ -84,6 +98,7 @@ export const reportDetailColumns = [
         title: "Кол-во продаж",
         dataIndex: "quantity",
         key: "quantity",
+        render: (value) => value || 0,
         sorter: (a, b) => a.ordersCount - b.ordersCount,
     },
     {
@@ -97,6 +112,7 @@ export const reportDetailColumns = [
         title: "Кол-во на складе",
         dataIndex: "quantityOnStock",
         key: "quantityOnStock",
+        render: (value) => value || 0,
         sorter: (a, b) => a.ordersCount - b.ordersCount,
     }
 ] as ColumnType<Required<any>>[];
